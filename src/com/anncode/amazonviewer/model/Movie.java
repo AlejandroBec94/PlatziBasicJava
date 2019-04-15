c71a0cccc6134b5c541d386d35329d5bbc1dd193
a.becerra <a.becor94@gmail.com>
2019-04-15 13:12:17 -0500

    Getters and Setters

--- src/com/anncode/amazonviewer/model/Movie.java
+++ src/com/anncode/amazonviewer/model/Movie.java
@@ -25,6 +25,71 @@ public class Movie {
         this.year = year;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public boolean isViewed() {
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void  setTitle(String title){
+        this.title = title;
+    }
+
     public void showData() {
         System.out.println("Titulo: " + title + " Genero: " + genre + " Año: " + year);
     }
