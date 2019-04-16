df8c49a919d3a4e47de380e86c0c2fb49b564961
a.becerra <a.becor94@gmail.com>
2019-04-16 13:52:10 -0500

    Collections

---
+++ src/com/anncode/amazonviewer/Collections.java
@@ -0,0 +1,38 @@
+package com.anncode.amazonviewer;
+
+import com.anncode.amazonviewer.model.Chapter;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class Collections {
+
+    public static void main(String[] args) {
+
+        final Set<Integer> hashSet = new HashSet<Integer>(1_000_000);
+        final Long startHashSetTime = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            hashSet.add(i);
+        }
+        final Long endHashSetTime = System.currentTimeMillis();
+        System.out.println("Time spent by HashSet: " + (endHashSetTime - startHashSetTime));
+
+        final Set<Integer> treeSet = new TreeSet<Integer>();
+        final Long startTreeSetTime = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            treeSet.add(i);
+        }
+        final Long endTreeSetTime = System.currentTimeMillis();
+        System.out.println("Time spent by TreeSet: " + (endTreeSetTime - startTreeSetTime));
+
+        final Set<Integer> linkedHashSet = new LinkedHashSet<Integer>(1_000_000);
+        final Long startLinkedHashSetTime = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            linkedHashSet.add(i);
+        }
+        final Long endLinkedHashSetTime = System.currentTimeMillis();
+        System.out.println("Time spent by LinkedHashSet: " + (endLinkedHashSetTime - startLinkedHashSetTime));
+
+    }
+
+}
